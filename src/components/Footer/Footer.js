a6bfdf8bffbddf54a144208edd4a77b57f859696
fg091102 <125964376+fg091102@users.noreply.github.com>
2024-08-28 19:43:10 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <div className={styles.container}>
+            Made with ♥ by Fenil Gandhi
+        </div>
+    )
+}
+
+export default Footer
